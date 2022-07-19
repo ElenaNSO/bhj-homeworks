abdd5dfed2540de6299dddd2f9b21073c0c4e0ad
ElenaNSO <yaelena@ngs.ru>
2022-07-19 15:57:04 +0700

    fix: change task.js

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -3,7 +3,8 @@ const MODAL_SUCCESS = document.querySelector('#modal_success');
 
 MODAL_MAINE.style.display = 'flex';
 
-let arrayModalMain = Array.from(document.querySelectorAll('.modal#modal_main'));
+let arrayModalMain = document.querySelectorAll('.modal#modal_main');
+let arrayModalSuccess = document.querySelectorAll('#modal_success');
 
 arrayModalMain[0].childNodes[1].childNodes[1].onclick = function(){
     MODAL_MAINE.style.display = 'none';   
@@ -11,4 +12,7 @@ arrayModalMain[0].childNodes[1].childNodes[1].onclick = function(){
 arrayModalMain[0].childNodes[1].childNodes[3].onclick = function(){
     MODAL_MAINE.style.display = 'none';  
     MODAL_SUCCESS.style.display = 'flex'; 
+}
+arrayModalSuccess[0].childNodes[1].childNodes[1].onclick = function(){
+    MODAL_SUCCESS.style.display = 'none';   
 }
