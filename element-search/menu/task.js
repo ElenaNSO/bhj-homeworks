abde6785389ea1ca5f334e9a227262e9b78bf069
ElenaNSO <yaelena@ngs.ru>
2022-07-19 15:59:21 +0700

    fix: change  task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,30 +1,30 @@
-let massMenuLink = Array.from(document.querySelectorAll('.menu__link'));
+let massMenuLink = document.querySelectorAll('.menu__link');
 let MENU_MAIN = document.querySelector('.menu_main');
   
-const toggleMenu= (element) => {
-    if(element.nextElementSibling.className === "menu menu_sub menu_active") {
-        element.nextElementSibling.classList.toggle('menu_active',false);
-    }
-    else if(element.nextElementSibling.className === "menu menu_sub") {
-        element.nextElementSibling.classList.toggle('menu_active',true);
-    }
-}
-
 const hideMenu = () => {
   massMenuLink.forEach(element => {
     if(element.nextElementSibling != null){
-        if(element.nextElementSibling.className === "menu menu_sub menu_active") {
-          element.nextElementSibling.classList.toggle('menu_active',false);
+        if(element.nextElementSibling.classList.contains("menu_active")) {
+          element.nextElementSibling.classList.remove('menu_active');
         }
       }
   })
 }
 
 MENU_MAIN.addEventListener('click', element => {
-  hideMenu();
-  toggleMenu(element.target);
+  let childName = element.target;
+  let parentEventList = childName.closest('.menu__item');
+ 
+  switch(parentEventList.children[1].className ) {
+    case "menu menu_sub menu_active":
+      parentEventList.children[1].classList.remove('menu_active');
+      break;
+    case "menu menu_sub":
+      hideMenu();
+      parentEventList.children[1].classList.add('menu_active');
+      break;
+      default:
+        break;
+  }
   element.preventDefault();
-  MENU_MAIN.removeEventListener("click", toggleMenu());
-  
 },false)
-
