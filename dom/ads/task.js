df1e40fb507a0cef31e83feb8f845bbc58454ae1
ElenaNSO <yaelena@ngs.ru>
2022-07-28 17:23:16 +0700

    fix: cnange ads/task.js

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -2,18 +2,17 @@ class Rotation {
     constructor(container){
         this.container = container;
         this.rotator = Array.from(document.querySelectorAll('.rotator__case'));//выборка элементов span
-        this.ActivRotatorElement = 0;//индекс массива
+        this.activRotatorElement = 0;//индекс массива
         this.speed = 0;//скорость
         this.change();
     }
 //задаем цвет и скорость для элемента
     changeString() {
-        this.speed =+this.rotator[this.ActivRotatorElement].dataset.speed
-        this.rotator[this.ActivRotatorElement].style.color = this.rotator[this.ActivRotatorElement].dataset.color
+        this.speed =+this.rotator[this.activRotatorElement].dataset.speed
+        this.rotator[this.activRotatorElement].style.color = this.rotator[this.activRotatorElement].dataset.color
     }
 
     change() {
-        let intervalId = null;
         //очищаем элементы от класса rotator__case_active
         this.rotator.forEach(element => {
             element.classList.remove('rotator__case_active');
@@ -21,13 +20,13 @@ class Rotation {
         });
 
         this.changeString();//красим и задаем скорость
-        this.rotator[this.ActivRotatorElement].classList.add('rotator__case_active');//выводим на экран
+        this.rotator[this.activRotatorElement].classList.add('rotator__case_active');//выводим на экран
            
-        intervalId = setInterval(() => {
+        setTimeout(() => {
             this.change()
         }, this.speed);
 
-        this.ActivRotatorElement = ((this.ActivRotatorElement + 1) % this.rotator.length);//меняем индекс элемента
+        this.activRotatorElement = ((this.activRotatorElement + 1) % this.rotator.length);//меняем индекс элемента
     }
 }
 
