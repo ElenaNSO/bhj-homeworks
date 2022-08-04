90c41ffb2dfcfd1d48bad9375e4994cc35163343
ElenaNSO <yaelena@ngs.ru>
2022-08-04 16:58:51 +0700

    feat: add text-editor/task.js

---
+++ client-state/text-editor/task.js
@@ -0,0 +1,18 @@
+const textArea = document.querySelector('#editor');
+const buttonTextArea = document.querySelector('.button-text-area');
+
+const storeName = localStorage.getItem('text');
+    if(storeName) {
+        textArea.textContent = storeName;
+    }
+
+    textArea.addEventListener('change', event => {
+
+    const textTextArea = textArea.value;
+    localStorage.setItem('text', textTextArea);
+})
+
+buttonTextArea.addEventListener('click', event => {
+    textArea.value = null;
+    localStorage.removeItem('text');
+})
